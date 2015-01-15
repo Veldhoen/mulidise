657a49da2889e0e3ae203dd10bc553af84462bc0
Veldhoen <sara.veldhoen+github@gmail.com>
2015-01-15 12:33:23 +0100

    working eval pipeline

--- ourCode/classifierTED.sh
+++ ourCode/classifierTED.sh
@@ -4,7 +4,7 @@ sara=/home/sveldhoen/mulidise
 experiment=$1
 embeddings=$benno/document-representations/data/embeddings/original-de-en.en
 
-mkdir $experiment/docEmbeddings
+mkdir -p $experiment/docEmbeddings
 mkdir $experiment/models
 mkdir $experiment/results
 
@@ -14,6 +14,7 @@ tedIDFs=$sara/idfsTED
 classifiers=$benno/document-representations/bin
 preprocess=$benno/mulidise/ourCode/preprocessData.py
 
+date
 echo preprocess data...
 python -u $preprocess\
        -d $tedDocs \
@@ -22,33 +23,43 @@ python -u $preprocess\
        -i $tedIDFs
 echo done.
 
+
+date
+echo training models...
 languages=(en de es fr it nl pb pl ro)
 topics=(art arts biology business creativity culture design economics education entertainment global health politics science technology)
 
 for lan in ${languages[@]}; do
-  echo $lan
   for topic in ${topics[@]}; do
-    echo $topic
+    echo -e '\t' $lan-$topic
     # train, i.e. create model:
     java  -ea -Xmx2000m -cp \
       $classifiers ApLearn  \
       --train-set  $experiment/docEmbeddings/$lan/train.$topic.emb \
       --model-name $experiment/models/$lan.$topic.model \
-      --epoch-num 10
+      --epoch-num 10 &
   done
 done
+echo done.
 
+wait
+
+date
+echo testing models...
 
 for lan1 in ${languages[@]}; do
-  otherLans=`echo ${languages[@]}| sed "s/\b$lan\b//g"`
-  for lan2 in ${otherLans[@]}; do
+#  otherLans=`echo ${languages[@]}| sed "s/\b$lan\b//g"`
+# for lan2 in ${otherLans[@]}; do
+  for lan2 in ${languages[@]}; do
     for topic in ${topics[@]}; do
+      echo -e '\t' $lan1-$lan2: $topic
       #test classifier of lan1 on lan2
       java  -ea -Xmx2000m -cp \
-        $classifiers ApClassify
+        $classifiers ApClassify \
         --test-set $experiment/docEmbeddings/$lan2/test.$topic.emb \
         --model-name $experiment/models/$lan.$topic.model \
-        > $experiment/results/$lan1-$lan2.$topic.result
+        > $experiment/results/$lan1-$lan2.$topic.result &
     done
   done
 done
+echo done.
