375999f9809032e427f691926da265e870a1aefc
Benno Kruit <bennokr@gmail.com>
2015-01-29 12:24:58 +0100

    Refactor final experiments, clean up old explorations

---
+++ ourCode/make_ted_docs_from_paragraphs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+from gensim.models.doc2vec import Doc2Vec, LabeledSentence
+import sys
+import timeit
+from parallel_bow import ParallelMergedLabeledLineSentence
+from ted_paragraph_docs import save_ted_docs, TedLabeledLineSentence
+from itertools import chain
+
+"""
+	usage: <corpus-root> <langs> <ted-dir> <export-dir>
+
+	open corpus (1 or 2 langs) & ted, init vocab
+	train softmax & sentences on corpus
+	train sentences on ted
+	export ted documents 
+"""
+corpus_root, langs, ted_dir, export_dir = sys.argv[1:]
+langs = langs.split(',')
+
+corpus_size = 50000
+size=256
+model = Doc2Vec(dm=0, alpha=0.025, min_alpha=0.025, size=256)
+corpus = ParallelMergedLabeledLineSentence(corpus_root, langs, corpus_size)
+ted = TedLabeledLineSentence(ted_dir)
+model.build_vocab(chain(ted, corpus))
+print '%s words & sents in vocab' % len(model.vocab)
+
+print 'epochs on corpus:'
+start = timeit.default_timer()
+for epoch in range(10):
+    model.train(corpus)
+    print epoch,
+    model.alpha -= 0.002  # decrease the learning rate
+print '(%ss)' % (timeit.default_timer() - start)
+
+print 'Stop training softmax!'
+model.train_words = False
+
+print 'epochs on ted:'
+model.alpha=0.025
+start = timeit.default_timer()
+for epoch in range(10):
+    model.train(ted)
+    print epoch,
+    model.alpha -= 0.002  # decrease the learning rate
+print '(%ss)' % (timeit.default_timer() - start)
+
+save_ted_docs(model, ted_dir, export_dir)
---
+++ ourCode/ted_paragraph_docs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+from gensim.models.doc2vec import Doc2Vec, LabeledLineSentence, LabeledSentence
+from itertools import izip, chain, islice
+import os
+import numpy as np
+from inspection import preprocess
+
+def ted_file_filter(root):
+	for (dirpath, dirnames, filenames) in os.walk(root):
+		for name in filenames:
+			if name.endswith('.ted'):
+				yield name, os.path.join(dirpath, name)
+
+
+def save_ted_docs(model, ted_dir, out_dir):
+    """
+        `model` has sentences in `<l1>`
+        `ted_dir` ends with `/<l1>-<l2>`
+    """
+    for name, f in ted_file_filter(ted_dir):
+        dataset, topic, posneg = f.split('/')[-4:-1]
+        with open(f) as lines:
+            vec = np.zeros(size)
+            for line in lines:
+                vec += model[line]
+        out = os.path.join(out_dir, '%s/%s' % (dataset, topic))
+        vecstr = ' '.join(['%s:%s' % (i, d) for i,d in enumerate(vec)])
+        if not os.path.exists(os.path.dirname(out)):
+            os.makedirs(os.path.dirname(out))
+        with open(out, 'a') as o:
+            o.write('%s %s\n' % (1+int(posneg=='negative'), vecstr))
+
+class TedLabeledLineSentence(object):
+    def __init__(self, ted_dir):
+        # make a dict of unique documents
+        self.ted_docs = {name:f for name, f in ted_file_filter(ted_dir)}
+ 
+    def __iter__(self):
+        for doc in self.ted_docs.values():
+            with open(doc) as lines:
+                for l in lines:
+                    l = preprocess(l) # ted corpus has lang suffixes
+                    yield LabeledSentence(words=l.split(), labels=[l])
